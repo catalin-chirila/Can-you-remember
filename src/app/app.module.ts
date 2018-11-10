6299f31649558a8eaaaf2821f34dddb36234a058
catalin-chirila <catalinchirila03@gmail.com>
2018-11-10 17:40:50 +0200

    fix info-bar and level styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ShapeComponent } from './shapes/shape/shape.component';
 import { ShapesToPickFromComponent } from './shapes/shapes-to-pick-from/shapes-to-pick-from.component';
@@ -10,8 +9,6 @@ import { TimerComponent } from './info-bar/timer/timer.component';
 import { InfoBarComponent } from './info-bar/info-bar.component';
 import { LevelComponent } from './level/level.component';
 import { LevelService } from './common/level.service';
-import { TimerService } from './common/timer.service';
-import { GameService } from './common/game.service';
 
 @NgModule({
   declarations: [
